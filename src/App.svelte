bf1f9113b4ec6c21eeb9441a1437ab6e4206c017
stearnsbq <stearnsbq@vcu.edu>
2022-07-10 23:16:51 -0400

    links :)

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,16 @@
     const links = [
         {url: 'https://twitter.com/llwlll__/', label: 'Twitter', icon: 'icons/twitter.svg'},
         {url: 'https://www.patreon.com/llwll/', label: 'Patreon', icon: 'icons/patreon.svg'},
-        {url: 'https://llwll.bandcamp.com/', label: 'Bandcamp', icon: 'icons/bandcamp.svg'},
-        {url: 'https://open.spotify.com/artist/6JwRFnmMxmWcYribpIJbcS', label: 'Spotify', icon: 'icons/spotify.svg'},
-        {url: 'https://soundcloud.com/llwll', label: 'Soundcloud', icon: 'icons/soundcloud.svg'},
+        {url: 'https://llwll.bandcamp.com/', label: 'Bandcamp', icon: 'icons/bandcamp.png'},
+        {url: 'https://open.spotify.com/artist/6JwRFnmMxmWcYribpIJbcS', label: 'Spotify', icon: 'icons/spotify.png'},
+        {url: 'https://soundcloud.com/llwll', label: 'Soundcloud', icon: 'icons/soundcloud.png'},
+        {url: 'https://www.instagram.com/llwlll_/?hl=en', label: 'Instagram', icon: 'icons/instagram.png'},
     ]
+
+
+    function onLinkClick(url: string){
+        window.open(url, "_blank")
+    }
 
 
 </script>
@@ -24,9 +30,9 @@
         <div class="artist-links">
             {#each links as link }
     
-            <div class="link">
+            <div class="link" on:click={() => onLinkClick(link.url)}>
                 <img src={link.icon} alt={link.label}>
-                <a href={link.url}>{link.label}</a>
+                <span>{link.label}</span>
             </div>
                 
             {/each}
@@ -43,8 +49,13 @@
 <style lang="scss">
 
     .artist-image{
+        display: flex;
+        justify-content: center;
         img{
+            height: 225px;
             border-radius: 50%;
+            border: 2px solid black;
+            box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
     }
 
@@ -56,12 +67,41 @@
         height: 100vh;
     }
 
+    .artist-content{
+        display: flex;
+        flex-direction: column;
+        gap: 50px;
+    }
+
     .artist-links{
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        
+        justify-content: center;
+        align-items: center;
 
         .link{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 50px;
+            cursor: pointer;
+            width: 250px;
+
+            border: 2px solid black;
+
+            border-radius: 15px;
+            padding: 25px;
+ 
             img{
                 height: 25px;
             }
+
+            span{
+
+            }
+
         }
 
 
